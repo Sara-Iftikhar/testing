e1607c1abf3e78de61e6e7b414abab9faa3fd226
Sara Iftikhar <sara.rwpk@gmail.com>
2022-10-09 03:41:08 +0900

    updated

--- scripts/eda.py
+++ scripts/eda.py
@@ -8,8 +8,12 @@ import site
 site.addsitedir(r"E:\AA\AI4Water")
 
 from ai4water.eda import EDA
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 from load_data import ads_df
+
+ads_df = ads_df.copy(deep=True)
 
 ads_df.pop("Adsorbent")
 ads_df.pop("Dye")
@@ -17,3 +21,7 @@ ads_df.pop("Dye")
 eda = EDA(data = ads_df, save=False)
 
 eda.correlation()
+
+# %%
+sns.pairplot(ads_df, hue='qe')
+plt.show()
