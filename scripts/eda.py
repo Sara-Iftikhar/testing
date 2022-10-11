fc687c0f0db3ddf59f71e7f0785fb18d26fee406
Sara Iftikhar <sara.rwpk@gmail.com>
2022-10-11 21:40:23 +0900

    fixed box plot plot

--- scripts/eda.py
+++ scripts/eda.py
@@ -6,13 +6,14 @@ EDA
 
 import site
 site.addsitedir(r"E:\AA\AI4Water")
+site.addsitedir(r"E:\AA\PDPbox1")
 
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 from ai4water.eda import EDA
-from easy_mpl import hist, box_plot
+from easy_mpl import hist
 
 from utils import ads_df
 
@@ -45,48 +46,50 @@ eda.correlation()
 
 # %%
 
-h_paras = ads_df.columns
+# h_paras = ads_df.columns
+#
+# fig, axes = plt.subplots(nrows=4, ncols=3,
+#                             figsize=(12, 12),
+#                         squeeze=False)
+#
+# if not isinstance(axes, np.ndarray):
+#     axes = np.array([axes])
+#
+# for ax, col, label  in zip(axes.flat, ads_df, h_paras):
+#
+#     sns.lineplot(ads_df[col], ax=ax,
+#                 palette = 'Spectral',
+#            #show=False,
+#          #title=label,
+#          )
+#     ax.legend(fontsize=10)
+# plt.legend()
+# plt.show()
 
-fig, axes = plt.subplots(nrows=4, ncols=3,
-                            figsize=(12, 12),
-                        squeeze=False)
+# %%
 
-if not isinstance(axes, np.ndarray):
-    axes = np.array([axes])
-
-for ax, col, label  in zip(axes.flat, ads_df, h_paras):
-
-    sns.lineplot(ads_df[col], ax=ax,
-                palette = 'Spectral',
-           #show=False,
-         #title=label,
-         )
-    ax.legend(fontsize=10)
-plt.legend()
+ax = sns.boxplot(ads_df)
+ax.set_xticks(np.arange(len(ads_df.columns)))
+ax.set_xticklabels(ads_df.columns, rotation=65)
 plt.show()
 
 # %%
 
-sns.boxplot(ads_df, )
-plt.show()
-
-# %%
-
-h_paras = ads_df.columns
-
-fig, axes = plt.subplots(nrows=4, ncols=3,
-                            figsize=(12, 12),
-                        squeeze=False)
-
-if not isinstance(axes, np.ndarray):
-    axes = np.array([axes])
-
-for ax, col, label  in zip(axes.flat, ads_df, h_paras):
-
-    hist(ads_df[col], ax=ax,
-           show=False,
-         title=label,
-         )
-    ax.legend(fontsize=10)
-plt.legend()
-plt.show()
+# h_paras = ads_df.columns
+#
+# fig, axes = plt.subplots(nrows=4, ncols=3,
+#                             figsize=(12, 12),
+#                         squeeze=False)
+#
+# if not isinstance(axes, np.ndarray):
+#     axes = np.array([axes])
+#
+# for ax, col, label  in zip(axes.flat, ads_df, h_paras):
+#
+#     hist(ads_df[col], ax=ax,
+#            show=False,
+#          title=label,
+#          )
+#     ax.legend(fontsize=10)
+# plt.legend()
+# plt.show()
