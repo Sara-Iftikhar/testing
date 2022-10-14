5b5281e576710c50649e97fd09c50518bd14651d
Sara Iftikhar <sara.rwpk@gmail.com>
2022-10-14 22:34:05 +0900

    replaced tf model with ai4water model

--- scripts/mlp.py
+++ scripts/mlp.py
@@ -9,85 +9,43 @@ site.addsitedir(r"E:\AA\AI4Water")
 
 import os
 
-import numpy as np
 import pandas as pd
-import tensorflow as tf
-from tensorflow import keras
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+from ai4water import Model
+from ai4water.models import MLP
+from ai4water.utils.utils import get_version_info
 from ai4water.postprocessing import LossCurve, ProcessPredictions
 from ai4water.utils.utils import dateandtime_now
 
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+from utils import get_data, evaluate_model, get_dataset
 
-from utils import get_data, reset_seed, evaluate_model
-
-reset_seed(313)
-
-print(np.__version__, tf.__version__)
+get_version_info()
 
 # %%
 
 X_train, y_train, X_test, y_test = get_data()
+ds ,  _, _ = get_dataset()
 
 # %%
 
 path = os.path.join(os.getcwd(),'results',f'mlp_{dateandtime_now()}')
 os.makedirs(path)
 
-inp = Input(shape=(68,))
-dense1 = Dense(units=37, activation="relu")(inp)
-dense2 = Dense(units=37, activation="relu")(dense1)
-dense3 = Dense(units=37, activation="relu")(dense2)
-dense4 = Dense(units=37, activation="relu")(dense3)
-dense5 = Dense(units=1)(dense4)
+model = Model(
+    model=MLP(units=37, num_layers=4,
+              activation='relu'),
+    lr=0.004561316449575947,
+    input_features=ds.input_features,
+    output_features=ds.output_features,
+    epochs=400, batch_size=24,
+)
 
 # %%
 
-model = Model(inputs=inp, outputs=dense5)
-
-# %%
-
-model.compile(optimizer=Adam(learning_rate=0.004561316449575947),
-              loss='mse')
-
-# %%
-
-print(model.summary())
-
-# %%
-_monitor = 'val_loss'
-fname = "{val_loss:.5f}.hdf5"
-
-callbacks = list()
-callbacks.append(keras.callbacks.ModelCheckpoint(
-    filepath=path + f"{os.sep}weights_" + "{epoch:03d}_" + fname,
-    save_weights_only=True,
-    monitor=_monitor,
-    mode='min',
-    save_best_only=True))
-
-callbacks.append(keras.callbacks.EarlyStopping(
-    monitor=_monitor, min_delta=0.001,
-    patience=100, verbose=0, mode='auto'
-))
-
-h = model.fit(x=X_train, y=y_train,
-          validation_data=(X_test, y_test),
-          epochs=400, batch_size=24,
-              callbacks=callbacks)
-
-# %%
-# find and update best weights
-best_weights = [f for f in os.listdir(path) if f.endswith(".hdf5")][-1]
-filepath = os.path.join(path, best_weights)
-model.load_weights(filepath)
-
-# %%
-LossCurve(path=path).plot_loss(h.history)
+model.fit(X_train,y_train,
+          validation_data=(X_test, y_test))
 
 # %%
 # Training data
--- scripts/utils.py
+++ scripts/utils.py
@@ -7,10 +7,8 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import KFold
 
-from tensorflow import keras
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.models import Model
-from tensorflow.keras.optimizers import Adam
+from ai4water import Model
+from ai4water.models import MLP
 
 from ai4water.preprocessing import DataSet
 from ai4water.utils.utils import dateandtime_now
@@ -54,14 +52,6 @@ def _make_data():
     ads_df_enc['qe'] = df1
     return ads_df_enc, adsorbent_enc, dye_encoder
 
-#ads_df_enc = ads_df_enc.drop([374])
-
-# regen_df_enc, _, _ = _ohe_encoder(regen_df, 'Adsorbent')
-# regen_df_enc = regen_df_enc.drop(columns='Adsorbent')
-# df1 = regen_df_enc.pop('efficiency_%')
-# regen_df_enc['efficiency_%'] = df1
-
-
 def get_dataset():
     ads_df_enc, adsorbent_encoder, dye_encoder = _make_data()
 
@@ -79,11 +69,7 @@ def get_data():
 
     dataset, _, _ = get_dataset()
 
-    # %%
-
     X_train, y_train = dataset.training_data()
-
-    # %%
 
     X_test, y_test = dataset.test_data()
     return X_train, y_train, X_test, y_test
@@ -97,82 +83,24 @@ def make_path():
 def get_fitted_model(return_path=False):
 
     X_train, y_train, X_test, y_test = get_data()
+    ds, _, _ = get_dataset()
 
     path = make_path()
 
-    reset_seed()
+    model = Model(
+        model=MLP(units=37, num_layers=4,
+                  activation='relu'),
+        lr=0.004561316449575947,
+        input_features=ds.input_features,
+        output_features=ds.output_features,
+        epochs=400, batch_size=24,
+    )
 
-    inp = Input(shape=(68,))
-    dense1 = Dense(units=37, activation="relu")(inp)
-    dense2 = Dense(units=37, activation="relu")(dense1)
-    dense3 = Dense(units=37, activation="relu")(dense2)
-    dense4 = Dense(units=37, activation="relu")(dense3)
-    dense5 = Dense(units=1)(dense4)
-
-    # %%
-
-    model = Model(inputs=inp, outputs=dense5)
-
-    # %%
-
-    model.compile(optimizer=Adam(learning_rate=0.004561316449575947),
-                  loss='mse')
-
-    callbacks = get_callbacks(path)
-
-    model.fit(x=X_train, y=y_train,
-                  validation_data=(X_test, y_test),
-                  epochs=400, batch_size=24,
-                  callbacks=callbacks, verbose=0)
-
-    # %%
-    update_model_with_best_weights(model, path)
+    model.fit(X_train, y_train)
 
     if return_path:
         return model, path
     return model
-
-
-def get_callbacks(path):
-    _monitor = 'val_loss'
-    fname = "{val_loss:.5f}.hdf5"
-    callbacks = list()
-    callbacks.append(keras.callbacks.ModelCheckpoint(
-        filepath=path + f"{os.sep}weights_" + "{epoch:03d}_" + fname,
-        save_weights_only=True,
-        monitor=_monitor,
-        mode='min',
-        save_best_only=True))
-
-    callbacks.append(keras.callbacks.EarlyStopping(
-        monitor=_monitor, min_delta=0.001,
-        patience=100, verbose=0, mode='auto'
-    ))
-    return callbacks
-
-
-def update_model_with_best_weights(model, path):
-    # find and update best weights
-    best_weights = [f for f in os.listdir(path) if f.endswith(".hdf5")][-1]
-    filepath = os.path.join(path, best_weights)
-    model.load_weights(filepath)
-    return
-
-
-def reset_seed(seed=313):
-    import numpy as np
-    np.random.seed(seed=313)
-
-    import tensorflow as tf
-    tf.random.set_random_seed(313)
-
-    import random
-    random.seed(313)
-
-    import os
-    os.environ['PYTHONHASHSEED'] = str(313)
-    return
-
 
 def confidenc_interval(model, X_train, y_train, X_test, y_test, alpha,
                     n_splits=5):
@@ -190,11 +118,9 @@ def confidenc_interval(model, X_train, y_train, X_test, y_test, alpha,
         return _df
 
     path = make_path()
-    callbacks = get_callbacks(path)
     model.fit(X_train, y_train, batch_size=24, verbose=0,
               validation_data=(X_test, y_test),
-              epochs=400, callbacks=callbacks)
-    update_model_with_best_weights(model, path)
+              epochs=400)
 
     residuals = y_train - model.predict(X_train)
     ci = np.quantile(residuals, 1 - alpha)
@@ -209,10 +135,8 @@ def confidenc_interval(model, X_train, y_train, X_test, y_test, alpha,
         y_train_, y_test_ = y_train[train_index], y_train[test_index]
 
         path = make_path()
-        callbacks = get_callbacks(path)
         model.fit(X_train_, y_train_, validation_data=(X_test_, y_test_),
-                  callbacks=callbacks, verbose=0, batch_size=24, epochs=400)
-        update_model_with_best_weights(model, path)
+                   verbose=0, batch_size=24, epochs=400)
 
         estimators.append(model)
         _pred = model.predict(X_test_)
