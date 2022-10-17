473e75fbf051a8ead9ac44cf1fbf74dc9ea0e61a
Sara Iftikhar <sara.rwpk@gmail.com>
2022-10-17 20:57:15 +0900

    improved regression plot

--- scripts/mlp.py
+++ scripts/mlp.py
@@ -6,9 +6,10 @@ MLP
 
 import site
 site.addsitedir(r"E:\AA\AI4Water")
+site.addsitedir(r"E:\AA\easy_mpl")
 
 import os
-
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -18,6 +19,9 @@ from ai4water.models import MLP
 from ai4water.utils.utils import get_version_info
 from ai4water.postprocessing import LossCurve, ProcessPredictions
 from ai4water.utils.utils import dateandtime_now
+from ai4water.utils import edf_plot
+from easy_mpl import plot, regplot
+from SeqMetrics import RegressionMetrics
 
 from utils import get_data, evaluate_model, get_dataset
 
@@ -40,16 +44,27 @@ model = Model(
     input_features=ds.input_features,
     output_features=ds.output_features,
     epochs=400, batch_size=24,
+    verbosity=0
 )
 
 # %%
 
-model.fit(X_train,y_train,
+h = model.fit(X_train,y_train,
           validation_data=(X_test, y_test))
+
+# %%
+
+ax = plot(h.history['loss'], show=False, label='training'
+                    , xlabel='Epochs', ylabel='Loss'
+                    )
+plot(h.history['val_loss'], ax=ax, label='validation',
+                xlabel='Epochs', ylabel='Loss'
+                )
 
 # %%
 # Training data
 # --------------
+
 train_p = model.predict(x=X_train,)
 
 # %%
@@ -60,8 +75,6 @@ evaluate_model(y_train, train_p)
 
 pp = ProcessPredictions(mode='regression', forecast_len=1,
                    path=path)
-
-pp.edf_plot(y_train, train_p, 'train', path)
 
 # %%
 
@@ -76,25 +89,24 @@ pp.residual_plot(pd.DataFrame(y_train), pd.DataFrame(train_p), 'train', path)
 
 # %%
 
-data_ = [pd.DataFrame(y_train, columns=['observed']), pd.DataFrame(train_p, columns=['predicted'])]
-data = pd.concat(data_, axis=1)
-
-sns.set_style("white")
-gridobj = sns.lmplot(x="observed", y="predicted",
-                     data=data,
-                     height=7, aspect=1.3, robust=True,
-                     scatter_kws=dict(s=150, linewidths=1.0, edgecolors='black',
-                                      marker="3",
-                                      alpha=0.5))
-gridobj.ax.set_xticklabels(gridobj.ax.get_xticklabels(), fontsize=20)
-gridobj.ax.set_yticklabels(gridobj.ax.get_yticklabels(), fontsize=20)
-gridobj.ax.set_xlabel(gridobj.ax.get_xlabel(), fontsize=24)
-gridobj.ax.set_ylabel(gridobj.ax.get_ylabel(), fontsize=24)
-plt.show()
+regplot(pd.DataFrame(y_train), pd.DataFrame(train_p), 'Training',
+        annotation_key='$R^2$',
+        annotation_val=RegressionMetrics(y_train,train_p).r2(),
+        marker_size=60,
+        marker_color='snow',
+        line_style='--',
+        line_color='indigo',
+        line_kws=dict(linewidth=3.0),
+        scatter_kws=dict(linewidths=1.1, edgecolors=np.array([56, 86, 199])/255,
+                         marker="8",
+                         alpha=0.7
+                         )
+        )
 
 # %%
 # Test data
-# --------------
+# ----------
+
 test_p = model.predict(x=X_test,)
 
 # %%
@@ -104,8 +116,6 @@ evaluate_model(y_test, test_p)
 # %%
 
 pp = ProcessPredictions(mode='regression', forecast_len=1, path=path)
-
-pp.edf_plot(y_test, test_p, 'test', path)
 
 # %%
 
@@ -120,19 +130,31 @@ pp.residual_plot(pd.DataFrame(y_test), pd.DataFrame(test_p), 'test', path)
 
 # %%
 
-data_ = [pd.DataFrame(y_test, columns=['observed']), pd.DataFrame(test_p, columns=['predicted'])]
-data = pd.concat(data_, axis=1)
+regplot(pd.DataFrame(y_test), pd.DataFrame(test_p), 'Test',
+        annotation_key='$R^2$',
+        annotation_val=RegressionMetrics(y_test,test_p).r2(),
+        marker_size=60,
+        marker_color='snow',
+        line_style='--',
+        line_color='indigo',
+        line_kws=dict(linewidth=3.0),
+        scatter_kws=dict(linewidths=1.1, edgecolors=np.array([56, 86, 199])/255,
+                         marker="8",
+                         alpha=0.7
+                         )
+        )
 
-sns.set_style("white")
-gridobj = sns.lmplot(x="observed", y="predicted",
-                     data=data,
-                     height=7, aspect=1.3, robust=True,
-                     scatter_kws=dict(s=150, linewidths=1.0, edgecolors='black',
-                                      marker="3",
-                                      alpha=0.5))
-gridobj.ax.set_xticklabels(gridobj.ax.get_xticklabels(), fontsize=20)
-gridobj.ax.set_yticklabels(gridobj.ax.get_yticklabels(), fontsize=20)
-gridobj.ax.set_xlabel(gridobj.ax.get_xlabel(), fontsize=24)
-gridobj.ax.set_ylabel(gridobj.ax.get_ylabel(), fontsize=24)
+# %%
+
+_, ax = plt.subplots(figsize=(5,4))
+
+edf_plot(np.abs(y_train-train_p), label='Training',
+        c=np.array([200, 49, 40])/255,
+         #c=np.array([234, 106, 41])/255,
+         linewidth=2.5,
+         show=False, ax=ax,)
+edf_plot(np.abs(y_test-test_p), xlabel='Absolute error',
+         c=np.array([68, 178, 205])/255, linewidth=2.5,
+         label='Test', ax=ax, show=False)
 plt.tight_layout()
 plt.show()
