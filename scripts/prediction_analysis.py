a16059ab1c8f04eff264e89dcfb3f3050fdbf867
Sara Iftikhar <sara.rwpk@gmail.com>
2022-10-17 21:31:08 +0900

    improved grids of prediction analysis

--- scripts/prediction_analysis.py
+++ scripts/prediction_analysis.py
@@ -5,15 +5,19 @@ Prediction Analysis
 """
 import site
 site.addsitedir(r"E:\AA\AI4Water")
+site.addsitedir(r"E:\AA\easy_mpl")
 
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from ai4water.postprocessing import prediction_distribution_plot, feature_interaction
+from ai4water.postprocessing import prediction_distribution_plot
 
 from utils import get_dataset, get_data, get_fitted_model, evaluate_model
 
+
 # %%
+
 X_train, y_train, X_test, y_test = get_data()
 
 # %%
@@ -24,7 +28,7 @@ dataset, _, _ = get_dataset()
 
 model = get_fitted_model()
 
- # %%
+# %%
 
 test_p = model.predict(x=X_test)
 
@@ -35,50 +39,84 @@ evaluate_model(y_test, test_p)
 # %%
 # Feature Interaction
 # --------------------
-_ = feature_interaction(
-    model.predict ,
-    X = pd.DataFrame(X_test, columns=dataset.input_features),
+
+_ = model.prediction_analysis(
+    x = pd.DataFrame(X_test, columns=dataset.input_features),
     features = ['Adsorption_time (min)', 'calcination (min)'],
     feature_names = ['Adsorption_time (min)', 'calcination (min)'],
-annotate=True, num_grid_points=None,
-)
-plt.show()
+    grid_types=["percentile", "percentile"],
+    num_grid_points=[6,6],
+    annotate_kws={'annotate_fontsize':15,
+                  'annotate_colors':np.array([['black', 'black', 'black', 'black'],
+                                              ['black', 'black', 'black', 'black'],
+                                              ['black', 'black', 'black', 'black'],
+                                              ['black', 'black', 'black', 'black'],
+                                              ['white', 'black', 'black', 'black']])}
+    )
+
 
 # %%
-_ = feature_interaction(
-    model.predict ,
-    X = pd.DataFrame(X_test, columns=dataset.input_features),
+_ = model.prediction_analysis(
+    x = pd.DataFrame(X_test, columns=dataset.input_features),
     features = ['Adsorption_time (min)', 'initial concentration'],
     feature_names = ['Adsorption_time (min)', 'initial concentration'],
-    annotate=True
+    grid_types=["percentile", "percentile"],
+    num_grid_points=[6, 6],
+    annotate_kws={'annotate_fontsize': 15,
+                  'annotate_colors': np.array([['black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black'],
+                                               ['white', 'black', 'black', 'black']])}
 )
-plt.show()
 
 # %%
-_ = feature_interaction(
-    model.predict ,
-    X = pd.DataFrame(X_test, columns=dataset.input_features),
+_ = model.prediction_analysis(
+    x = pd.DataFrame(X_test, columns=dataset.input_features),
     features = ['Adsorption_time (min)', 'solution pH'],
     feature_names = ['Adsorption_time (min)', 'solution pH'],
-    annotate=True
+    grid_types=["percentile", "percentile"],
+    num_grid_points=[6, 6],
+    annotate_kws={'annotate_fontsize': 15,
+                  'annotate_colors': np.array([['black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black'],
+                                               ['white', 'black', 'black', 'black']])}
 )
-plt.show()
+
 
 # %%
-_ = feature_interaction(
-    model.predict ,
-    X = pd.DataFrame(X_test, columns=dataset.input_features),
+_ = model.prediction_analysis(
+    x = pd.DataFrame(X_test, columns=dataset.input_features),
     features = ['Adsorption_time (min)', 'adsorbent loading '],
     feature_names = ['Adsorption_time (min)', 'adsorbent loading '],
-    annotate=True
+    grid_types=["percentile", "percentile"],
+    num_grid_points=[6, 6],
+    annotate_kws={'annotate_fontsize': 15,
+                  'annotate_colors': np.array([['black', 'black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black', 'black'],
+                                               ['black', 'black', 'black', 'black', 'white'],
+                                               ['black', 'black', 'black', 'black', 'white'],
+                                               ['black', 'black', 'black', 'black', 'black']])
+                                 }
 )
-plt.show()
 
 # %%
 # Prediction Distribution
 # ------------------------
-_ = out = prediction_distribution_plot(mode='regression',
-                             inputs=pd.DataFrame(X_test, columns=dataset.input_features),
-                             prediction=test_p,
-                             feature='Adsorption_time (min)',
-                             feature_name='Adsorption_time (min)')
+
+# ax, df = prediction_distribution_plot(mode='regression',
+#                              inputs=pd.DataFrame(X_test, columns=dataset.input_features),
+#                              prediction=test_p,
+#                              feature='Adsorption_time (min)',
+#                              feature_name='Adsorption_time (min)')
+#
+# preds = {}
+# for interval in df['display_column']:
+#     st, en = interval.split(',')  # todo
+#     df1 = pd.DataFrame(X_test, columns=dataset.input_features)
+#     df1['target'] = test_p
+#     df1 = df1[['Adsorption_time (min)', 'target']]
+#     df1 = df1[df1['Adsorption_time (min)']>0 & df1['Adsorption_time (min)']<20]
+#     preds[interval]  = df1['target'].values
